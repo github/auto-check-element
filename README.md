594b3b9399162cc41b8295d2645a0789a791e7cd
Kristján Oddsson <koddsson@gmail.com>
2018-04-05 15:07:29 +0100

    extract comment as a README

---
+++ README.md
@@ -0,0 +1,17 @@
+Input element that validates the value provided against
+a endpoint. Provide a URL and a CSRF token and the autocheck
+component will show validation confirmations and validation errors.
+
+The endpoint should return:
+ - a 200 HTTP status code if the provided value if valid.
+ - a 422 HTTP status code if the provided value is invalid.
+ - a optional error message in the body and a `Content-Type` header
+   with a value of `text/html; fragment`.
+
+```html
+<auto-check src='/signup_check/username' csrf="<%= authenticity_token_for("/signup_check/username") %>">
+ <input></input>
+</auto-check>
+```
+
+The component will attach event listeners to the first input child element.
--- src/index.js
+++ src/index.js
@@ -12,23 +12,6 @@ class XHRError extends Error {
   }
 }
 
-/*
- * Enhancement to a input element that validates the input against
- * a endpoint. Provide a URL and a CSRF token and the autocheck
- * component will show validation confirmations and validation errors.
- *
- * The endpoint should return:
- *  - a 200 HTTP status code if the provided value if valid.
- *  - a 422 HTTP status code if the provided value is invalid.
- *  - a optional error message in the body and a `Content-Type` header
- *    with a value of `text/html; fragment`.
- *
- * <auto-check src='/signup_check/username' csrf="<%= authenticity_token_for("/signup_check/username") %>">
- *  <input></input>
- * </auto-check>
- *
- * The component will attach event listeners to the first input child element.
- */
 class AutoCheckElement extends HTMLElement {
   constructor() {
     super()
