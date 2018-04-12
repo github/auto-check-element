ec0a0fbc15543ae879cd03b71f02886a27ec5231
Kristján Oddsson <koddsson@gmail.com>
2018-04-12 13:31:41 +0100

    fix eslint config and eslint errors

--- src/debounce.js
+++ src/debounce.js
@@ -1,6 +1,7 @@
 export default function debounce(callback, wait) {
   let timeout
   return function debounced(...args) {
+    /* eslint-disable-next-line no-invalid-this */
     const self = this
     function later() {
       clearTimeout(timeout)
