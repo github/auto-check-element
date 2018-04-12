9484fc99088450602f89fc4ead94d927c966ec6c
Kristján Oddsson <koddsson@gmail.com>
2018-04-12 14:48:43 +0100

    add more lifecycle event tests

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,3 +1,17 @@
+function checker(request, response, next) {
+  if (request.method === 'POST' && request.url.startsWith('/fail')) {
+    response.setHeader('Content-Type', 'text/html; fragment')
+    response.writeHead(422)
+    response.end('This is a error')
+    return
+  } else if (request.method === 'POST' && request.url.startsWith('/success')) {
+    response.writeHead(200)
+    response.end('This is a warning')
+    return
+  }
+  next()
+}
+
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
@@ -9,6 +23,13 @@ module.exports = function(config) {
     browsers: ['ChromeHeadless'],
     autoWatch: false,
     singleRun: true,
-    concurrency: Infinity
+    concurrency: Infinity,
+    middleware: ['checker'],
+    plugins: [
+      'karma-*',
+      {
+        'middleware:checker': ['value', checker]
+      }
+    ]
   })
 }
--- test/test.js
+++ test/test.js
@@ -10,4 +10,94 @@ describe('auto-check element', function() {
       assert.equal('AUTO-CHECK', el.nodeName)
     })
   })
+
+  describe('requesting server results', function() {
+    beforeEach(function() {
+      const container = document.createElement('div')
+      container.innerHTML = `
+        <auto-check csrf="foo" src="/success">
+          <input />
+        </auto-check>`
+      document.body.append(container)
+    })
+
+    afterEach(function() {
+      document.body.innerHTML = ''
+    })
+
+    it('emits a send event on input', function(done) {
+      const input = document.querySelector('input')
+      input.value = 'hub'
+      input.dispatchEvent(new InputEvent('input'))
+      input.addEventListener('autocheck:send', () => {
+        done()
+      })
+    })
+
+    it('emits a send event on change', function(done) {
+      const input = document.querySelector('input')
+      input.value = 'hub'
+      input.dispatchEvent(new InputEvent('change'))
+      input.addEventListener('autocheck:send', () => {
+        done()
+      })
+    })
+
+    it('emits a success event when server returns a non error response', function(done) {
+      const input = document.querySelector('input')
+      input.value = 'hub'
+      input.dispatchEvent(new InputEvent('change'))
+      input.addEventListener('autocheck:success', () => {
+        done()
+      })
+    })
+
+    it('emits a success event with warning when server returns a non error response', function() {
+      return new Promise(resolve => {
+        const input = document.querySelector('input')
+        input.value = 'hub'
+        input.dispatchEvent(new InputEvent('change'))
+        input.addEventListener('autocheck:success', event => {
+          resolve(event.detail.warning)
+        })
+      }).then(result => {
+        assert.equal('This is a warning', result)
+      })
+    })
+
+    it('emits a error event when server returns a error response', function(done) {
+      const autoCheck = document.querySelector('auto-check')
+      const input = document.querySelector('input')
+      autoCheck.src = '/fail'
+      input.value = 'hub'
+      input.dispatchEvent(new InputEvent('change'))
+      input.addEventListener('autocheck:error', () => {
+        done()
+      })
+    })
+
+    it('emits a error event when server returns a error response', function() {
+      return new Promise(resolve => {
+        const autoCheck = document.querySelector('auto-check')
+        const input = document.querySelector('input')
+        autoCheck.src = '/fail'
+        input.value = 'hub'
+        input.dispatchEvent(new InputEvent('change'))
+        input.addEventListener('autocheck:error', () => {
+          resolve(event.detail.message)
+        })
+      }).then(result => {
+        assert.equal('This is a error', result)
+      })
+    })
+
+    it('emits a complete event at the end of the lifecycle', function(done) {
+      const input = document.querySelector('input')
+      input.value = 'hub'
+      input.dispatchEvent(new InputEvent('change'))
+      input.addEventListener('autocheck:complete', () => {
+        done()
+      })
+    })
+  })
 })
