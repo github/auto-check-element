6013875edff90df03abededf735b1cc6805752b5
Kristján Oddsson <koddsson@gmail.com>
2018-04-12 11:56:16 +0100

    combine rollup configs into one

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,17 @@ const pkg = require('./package.json')
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: pkg['module'],
-    format: 'es'
-  },
+  output: [
+    {
+      file: pkg['module'],
+      format: 'es'
+    },
+    {
+      file: pkg['main'],
+      format: 'umd',
+      name: 'AutoCheckElement'
+    }
+  ],
   plugins: [
     babel({
       plugins: ['transform-custom-element-classes'],
