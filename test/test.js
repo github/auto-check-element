88038f2b64bdfa4fbec3e925963fa67cb17dfa10
Mu-An Chiou <me@muanchiou.com>
2020-05-22 18:06:13 -0400

    Fix prettier

--- test/test.js
+++ test/test.js
@@ -1,27 +1,27 @@
-describe('auto-check element', function () {
-  describe('element creation', function () {
-    it('creates from document.createElement', function () {
+describe('auto-check element', function() {
+  describe('element creation', function() {
+    it('creates from document.createElement', function() {
       const el = document.createElement('auto-check')
       assert.equal('AUTO-CHECK', el.nodeName)
     })
 
-    it('creates from constructor', function () {
+    it('creates from constructor', function() {
       const el = new window.AutoCheckElement()
       assert.equal('AUTO-CHECK', el.nodeName)
     })
 
-    it('has the correct attributes', function () {
+    it('has the correct attributes', function() {
       const el = document.createElement('auto-check')
       assert.equal(el.getAttribute('autocomplete', 'off'))
       assert.equal(el.getAttribute('spellcheck', 'false'))
     })
   })
 
-  describe('required attribute', function () {
+  describe('required attribute', function() {
     let checker
     let input
 
-    beforeEach(function () {
+    beforeEach(function() {
       const container = document.createElement('div')
       container.innerHTML = `
         <auto-check csrf="foo" src="/success" required>
@@ -33,44 +33,44 @@ describe('auto-check element', function () {
       input = checker.querySelector('input')
     })
 
-    afterEach(function () {
+    afterEach(function() {
       document.body.innerHTML = ''
       checker = null
       input = null
     })
 
-    it('invalidates empty input', function () {
+    it('invalidates empty input', function() {
       assert.isTrue(input.hasAttribute('required'))
       assert.isFalse(input.checkValidity())
     })
 
-    it('invalidates the input element on keypress', async function () {
+    it('invalidates the input element on keypress', async function() {
       const inputEvent = once(input, 'input')
       triggerInput(input, 'hub')
       await inputEvent
       assert.isFalse(input.checkValidity())
     })
 
-    it('invalidates input request is in-flight', async function () {
+    it('invalidates input request is in-flight', async function() {
       triggerInput(input, 'hub')
       await once(checker, 'loadstart')
       assert.isFalse(input.checkValidity())
     })
 
-    it('invalidates input with failed server response', async function () {
+    it('invalidates input with failed server response', async function() {
       checker.src = '/fail'
       triggerInput(input, 'hub')
       await once(input, 'auto-check-complete')
       assert.isFalse(input.checkValidity())
     })
 
-    it('validates input with successful server response', async function () {
+    it('validates input with successful server response', async function() {
       triggerInput(input, 'hub')
       await once(input, 'auto-check-complete')
       assert.isTrue(input.checkValidity())
     })
 
-    it('customizes the in-flight message', async function () {
+    it('customizes the in-flight message', async function() {
       checker.src = '/fail'
       const send = new Promise(resolve => {
         input.addEventListener('auto-check-start', event => {
@@ -84,7 +84,7 @@ describe('auto-check element', function () {
       assert.equal('Checking with server', input.validationMessage)
     })
 
-    it('customizes the error message', async function () {
+    it('customizes the error message', async function() {
       checker.src = '/fail'
       const error = new Promise(resolve => {
         input.addEventListener('auto-check-error', event => {
@@ -98,7 +98,7 @@ describe('auto-check element', function () {
       assert.equal('A custom error', input.validationMessage)
     })
 
-    it('skips validation if required attribute is not present', async function () {
+    it('skips validation if required attribute is not present', async function() {
       checker.src = '/fail'
       checker.required = false
       input.value = 'hub'
@@ -109,11 +109,11 @@ describe('auto-check element', function () {
     })
   })
 
-  describe('network lifecycle events', function () {
+  describe('network lifecycle events', function() {
     let checker
     let input
 
-    beforeEach(function () {
+    beforeEach(function() {
       const container = document.createElement('div')
       container.innerHTML = `
         <auto-check csrf="foo" src="/success">
@@ -125,13 +125,13 @@ describe('auto-check element', function () {
       input = checker.querySelector('input')
     })
 
-    afterEach(function () {
+    afterEach(function() {
       document.body.innerHTML = ''
       checker = null
       input = null
     })
 
-    it('emits network events in order', async function () {
+    it('emits network events in order', async function() {
       const events = []
       const track = event => events.push(event.type)
 
@@ -148,11 +148,11 @@ describe('auto-check element', function () {
     })
   })
 
-  describe('auto-check lifecycle events', function () {
+  describe('auto-check lifecycle events', function() {
     let checker
     let input
 
-    beforeEach(function () {
+    beforeEach(function() {
       const container = document.createElement('div')
       container.innerHTML = `
         <auto-check csrf="foo" src="/success">
@@ -164,48 +164,48 @@ describe('auto-check element', function () {
       input = checker.querySelector('input')
     })
 
-    afterEach(function () {
+    afterEach(function() {
       document.body.innerHTML = ''
       checker = null
       input = null
     })
 
-    it('emits auto-check-send on input', function (done) {
+    it('emits auto-check-send on input', function(done) {
       input.addEventListener('auto-check-send', () => done())
       input.value = 'hub'
       input.dispatchEvent(new InputEvent('input'))
     })
 
-    it('emits auto-check-send on change', function (done) {
+    it('emits auto-check-send on change', function(done) {
       input.addEventListener('auto-check-send', () => done())
       triggerInput(input, 'hub')
     })
 
-    it('emits auto-check-start on input', function (done) {
+    it('emits auto-check-start on input', function(done) {
       input.addEventListener('auto-check-start', () => done())
       input.value = 'hub'
       input.dispatchEvent(new InputEvent('input'))
     })
 
-    it('emits auto-check-start on change', function (done) {
+    it('emits auto-check-start on change', function(done) {
       input.addEventListener('auto-check-start', () => done())
       triggerInput(input, 'hub')
     })
 
-    it('emits auto-check-send 300 milliseconds after keypress', function (done) {
+    it('emits auto-check-send 300 milliseconds after keypress', function(done) {
       input.addEventListener('auto-check-send', () => done())
       input.value = 'hub'
       input.dispatchEvent(new InputEvent('input'))
     })
 
-    it('emits auto-check-success when server responds with 200 OK', async function () {
+    it('emits auto-check-success when server responds with 200 OK', async function() {
       triggerInput(input, 'hub')
       const event = await once(input, 'auto-check-success')
       const result = await event.detail.response.text()
       assert.equal('This is a warning', result)
     })
 
-    it('emits auto-check-error event when server returns an error response', async function () {
+    it('emits auto-check-error event when server returns an error response', async function() {
       checker.src = '/fail'
       triggerInput(input, 'hub')
       const event = await once(input, 'auto-check-error')
@@ -213,12 +213,12 @@ describe('auto-check element', function () {
       assert.equal('This is an error', result)
     })
 
-    it('emits auto-check-complete event at the end of the lifecycle', function (done) {
+    it('emits auto-check-complete event at the end of the lifecycle', function(done) {
       input.addEventListener('auto-check-complete', () => done())
       triggerInput(input, 'hub')
     })
 
-    it('emits auto-check-send event before checking if there is a duplicate request', function (done) {
+    it('emits auto-check-send event before checking if there is a duplicate request', function(done) {
       let counter = 2
       input.addEventListener('auto-check-send', () => {
         if (counter === 2) {
@@ -233,7 +233,7 @@ describe('auto-check element', function () {
       input.dispatchEvent(new InputEvent('input'))
     })
 
-    it('do not emit if essential attributes are missing', async function () {
+    it('do not emit if essential attributes are missing', async function() {
       const events = []
       checker.removeAttribute('src')
       input.addEventListener('auto-check-start', event => events.push(event.type))
@@ -242,12 +242,12 @@ describe('auto-check element', function () {
     })
   })
 
-  describe('csrf support', function () {
-    afterEach(function () {
+  describe('csrf support', function() {
+    afterEach(function() {
       document.body.innerHTML = ''
     })
 
-    it('fetches CSRF tokens from attributes', function () {
+    it('fetches CSRF tokens from attributes', function() {
       const container = document.createElement('div')
       container.innerHTML = `
         <auto-check csrf="foo" src="/success" required>
@@ -258,7 +258,7 @@ describe('auto-check element', function () {
       assert.equal(autoCheck.csrf, 'foo')
     })
 
-    it('fetches CSRF tokens from child elements', function () {
+    it('fetches CSRF tokens from child elements', function() {
       const container = document.createElement('div')
       container.innerHTML = `
         <auto-check src="/success" required>
